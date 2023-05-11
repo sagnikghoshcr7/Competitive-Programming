f15efc8617895d64aaa76ad67bf15a22886cb0c3
sagnikghoshcr7 <sagnikghoshmandra@gmail.com>
2023-05-12 00:21:00 +0530

    another easy peasy solution

---
+++ Practice Questions/D_Scope.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.*;
+import java.lang.*;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.io.*;
+
+public class D_Scope {
+    private static int arrMax(int[] A) {return Arrays.stream(A).max().getAsInt();}
+    private static int arrMin(int[] A) {return Arrays.stream(A).min().getAsInt();}
+    private static int arrSum(int[] A) {return Arrays.stream(A).sum();}
+    private static int countNumInArr(int[] A, int n) {return (int) Arrays.stream(A).filter(x -> x == n).count();}
+    private static void swap(int[] A, int i, int j) { int temp = A[i]; A[i] = A[j]; A[j] = temp; }
+    private static void reverse(int[] A) {int s=0,e=A.length-1;while(s<e){swap(A,s,e);s++;e--;}}
+    private static void reverse(int[] A, int s) {int e=A.length-1;while(s<e){swap(A,s,e);s++;e--;}}
+    private static void reverse(int[] A, int s, int e) {while(s<e){swap(A,s,e);s++;e--;}}
+
+    private static int countSetBits(int number){int count=0; while(number>0){++count; number &= number-1;} return count;}
+
+    private static boolean isEven(int i) {return (i & 1) == 0;}
+    private static boolean isPrime(int x) {if(x==1) return false; for(int i=2; i*i<=x; i++){if(x%i==0) return false;} return true;}
+
+    private static int gcd(int a, int b) {if (b == 0) return a; return gcd(b, a % b);}
+    private static int lcm(int a, int b) {return (a*b)/gcd(a, b);}
+    private static int[] listToArr(List<Integer> x) {return x.stream().mapToInt(i -> i).toArray();}
+
+    public static int[] setArray(int n) {int A[]=new int[n]; for(int i=0;i<n;i++) A[i]=sc.nextInt(); return A;}
+    public static long[] lsetArray(int n) {long A[]=new long[n]; for(int i=0;i<n;i++) A[i]=sc.nextLong(); return A;}
+
+    private static void prtArr(int[] A) {System.out.println(Arrays.toString(A));}
+    private static void prtArrWithSpce(int[] A) {for(int i=0;i<A.length;i++)System.out.print(A[i]+" ");}
+    private static void prtArrWithSpce(int[] A, int s) {for(int i=s;i<A.length;i++)System.out.print(A[i]+" ");}
+    private static void prtArrWithSpce(int[] A, int s, int e) {for(int i=s;i<=e;i++)System.out.print(A[i]+" ");}
+
+    // DecimalFormat df = new DecimalFormat("#.###");
+    // DecimalFormat df = new DecimalFormat(); df.setMaximumFractionDigits(12);
+    // System.out.println(df.format(input_Decimal_Here));
+
+    static Scanner sc = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+    public static void main(String[] args) { sagnik(); }
+    // public static void main(String[] args) { int t = sc.nextInt(); while(t-->0) sagnik(); }
+
+    private static void sagnik() {
+        String s = sc.next();
+        Stack<Character> st = new Stack<>();
+        HashSet<Character> hs = new HashSet<>();
+ 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                while (true) {
+                    char rmv = st.pop();
+                    if (rmv == '(') break;
+                    hs.remove(rmv);
+                }
+            } else {
+                if (hs.contains(s.charAt(i))) {
+                    out.println("No");
+                    out.flush();
+                    return;
+                }
+                hs.add(s.charAt(i));
+                st.push(s.charAt(i));
+            }
+        }
+ 
+        out.println("Yes");
+        out.flush();
+    }
+}
