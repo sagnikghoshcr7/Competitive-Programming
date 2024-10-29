b323ed1cca6bc7b96a4b2b3efcb90a705986c988
sagnikghoshcr7 <sagnikghoshmandra@gmail.com>
2024-10-29 10:19:34 +0530

    add Concert_Tickets solution

--- Practice Questions/CSES/Main.java
+++ Practice Questions/CSES/Main.java
@@ -1,38 +1,98 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		Reader io = new Reader();
+		PrintWriter pw = new PrintWriter(System.out);
 
-		int n = sc.nextInt();
-		int x = sc.nextInt();
-		sc.nextLine();
+		int ticketNum = io.nextInt();
+		int peopleNum = io.nextInt();
+		// No multiset in java so we'll have to use a TreeMap
+		NavigableMap<Integer, Integer> ticketMultiset = new TreeMap<>();
+		Map.Entry<Integer, Integer> val;
 
-		// Read in weights of children (this optimization is necessary to pass
-		// all test cases)
-		String[] weightsStr = sc.nextLine().split(" ");
-		// weights is of type Integer to prevent having to cast the result from
-		// Integer.parseInt() to an int, saving time (part of optimization)
-		Integer[] weights = new Integer[n];
-		for (int i = 0; i < n; i++) { weights[i] = Integer.parseInt(weightsStr[i]); }
-		Arrays.sort(weights);
-
-		int ans = 0;
-		int i = 0;      // left pointer
-		int j = n - 1;  // right pointer
-		while (i <= j) {
-			ans++;  // Increment number of gondolas used
-			if (i == j) break;
-			// If total weight is too large, move to lighter child
-			if (weights[i] + weights[j] > x) {
-				j--;
-			}
-			// Otherwise, we pair the two children and move on
-			else {
-				i++;
-				j--;
+		for (int i = 0; i < ticketNum; i++) {
+			int priceOfTicket = io.nextInt();
+			/*
+			 * If the set already contains the same key value,
+			 * create another pair next to it
+			 */
+			if (ticketMultiset.containsKey(priceOfTicket)) {
+				ticketMultiset.put(priceOfTicket,
+				                   ticketMultiset.get(priceOfTicket) + 1);
+			} else {
+				ticketMultiset.put(priceOfTicket, 1);
 			}
 		}
-		System.out.println(ans);
+
+		for (int i = 0; i < peopleNum; i++) {
+			int customerMaxPrice = io.nextInt();
+			/*
+			 * Get the optimal ticket for this customer
+			 * We add 1 because lowerEntry returns the highest
+			 * *strictly* lower entry
+			 */
+			val = ticketMultiset.lowerEntry(customerMaxPrice + 1);
+			// If there are no possible prices, we can exit and return -1
+			if (val != null) {
+				pw.println(val.getKey());
+				/*
+				 * If there's more than one set with the key value,
+				 * then replace the current set with
+				 * the next-lowest set with the same key
+				 */
+				if (val.getValue() == 1) {
+					ticketMultiset.remove(val.getKey());
+
+				} else {
+					ticketMultiset.put(val.getKey(), val.getValue() - 1);
+				}
+			} else {
+				pw.println(-1);
+			}
+		}
+		io.close();
+		pw.close();
 	}
+	// Kattio is too slow
+	// BeginCodeSnip{Fast Reader}
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1) buffer[0] = -1;
+		}
+
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead) fillBuffer();
+			return buffer[bufferPointer++];
+		}
+
+		public void close() throws IOException {
+			if (din == null) return;
+			din.close();
+		}
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ') c = read();
+			boolean neg = (c == '-');
+			if (neg) c = read();
+			do { ret = ret * 10 + c - '0'; } while ((c = read()) >= '0' && c <= '9');
+
+			if (neg) return -ret;
+			return ret;
+		}
+	}
+	// EndCodeSnip{}
 }
